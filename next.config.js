aaccea63e796ec46e098b06c83c7a1e8ce3d85a8
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-09 10:40:44 +0300

    fix:   `next export` has been removed in favor of 'output: export' in next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production';
+/** @type {import('next').NextConfig} */
 
 module.exports = {
+    output: 'export',
     assetPrefix: isProd ? '/vistamart_hgn11' : '',
     basePath: isProd ? '/vistamart_hng11' : '',
     images: {
